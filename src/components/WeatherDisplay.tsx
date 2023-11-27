e1ef5a55773d31d5e462a14dbca068383256ce7c
muhamedgit <hameactros@gmail.com>
2023-11-27 23:51:47 +0100

    fix refactoring

--- src/components/WeatherDisplay.tsx
+++ src/components/WeatherDisplay.tsx
@@ -1,109 +1,110 @@
 import { useEffect, useState } from 'react';
-import { getCurentWeatherData } from './Weather';
+import { getCurentWeatherData } from '../lib/WeatherService';
 import '../App.css';
 
-interface WeatherData {
-  location: {
-    name: string;
-  };
-  current: {
-    temp_c: number;
-    condition: {
-      code: number;
-      icon: string;
-      text: string;
-    };
-  };
-  forecast: {
-    forecastday: [
-      daynum: {
-        date: Date;
-        day: {
-          maxtemp_c: number;
-          mintemp_c: number;
-          condition: {
-            code: number;
-            icon: string;
-            text: string;
-          };
-        };
-      }
-    ];
-  };
-}
+type Current = {
+  currentTemperature: string;
+  icon: string;
+  text: string;
+};
+
+type ForecastDay = {
+  maxTemp: number;
+  minTemp: number;
+  icon: string;
+  text: string;
+  date: Date;
+};
 
 function WeatherDisplay() {
-  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
-  const [loading, setLoading] = useState(true);
+  const [currentWeather, setCurrentWeather] = useState<Current>();
+  const [location, setLocation] = useState<string>('Maribor');
+  const [forecast, setForecast] = useState<Array<ForecastDay>>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchWeatherData = async (location: string) => {
+    try {
+      const data = await getCurentWeatherData(location);
+
+      setCurrentWeather({
+        currentTemperature: data.current.temp_c,
+        icon: data.current.condition.icon,
+        text: data.current.condition.text,
+      });
+      const forecast = data.forecast.forecastday;
+      const newForecast: Array<ForecastDay> = [];
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      forecast.map((item: any) => {
+        newForecast.push({
+          maxTemp: item.day.maxtemp_c,
+          minTemp: item.day.mintemp_c,
+          icon: item.day.condition.icon,
+          text: item.day.condition.text,
+          date: item.date,
+        });
+      });
+      setForecast(newForecast);
+      console.log('Received Weather Data:', forecast);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.error('Error fetching data:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchWeatherData = async (location: string) => {
-      try {
-        const data = await getCurentWeatherData(location);
-        console.log('Received Weather Data:', data);
-        setWeatherData(data);
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    const location = 'Maribor';
     console.log('Fetching weather for:', location);
     fetchWeatherData(location);
-  }, []);
+  }, [location]);
 
   return (
-    <div>
-      {loading ? (
-        <p>Loading...</p>
-      ) : weatherData ? (
+    <>
+      <form>
+        <input
+          type='text'
+          onChange={(e) => {
+            setLocation(e.target.value);
+          }}
+        />
+      </form>
+      {!isLoading && (
         <div>
           <h2>Current Weather</h2>
           <div className='Curent-Container'>
-            {weatherData.location && (
-              <p>Location: {weatherData.location.name}</p>
-            )}
-            {weatherData.current && (
-              <p>Temperature: {weatherData.current.temp_c} °C</p>
-            )}
-            {
-              <div>
-                <img
-                  src={weatherData.current.condition.icon}
-                  alt=''
-                />
-                <p>{weatherData.current.condition.text}</p>
-              </div>
-            }
+            <p>Location: {location}</p>
+            <p>Temperature: {currentWeather?.currentTemperature} °C</p>
+            <div>
+              <img
+                src={currentWeather?.icon}
+                alt=''
+              />
+              <p>{currentWeather?.text}</p>
+            </div>
           </div>
 
           <h2>Forecast Days</h2>
           <div className='forecast-container'>
-            {weatherData.forecast &&
-              weatherData.forecast.forecastday.map((day, index) => (
-                <div
-                  key={index}
-                  className='forecast-day'>
-                  <div>
-                    <p>Date: {new Date(day.date).toLocaleDateString()}</p>
-                    <p>Max Temp: {day.day.maxtemp_c} °C</p>
-                    <p>Min Temp: {day.day.mintemp_c} °C</p>
-                    <img
-                      src={day.day.condition.icon}
-                      alt=''
-                    />
-                    <p>{day.day.condition.text}</p>
-                  </div>
+            {forecast?.map((item: ForecastDay, itemIdx: number) => (
+              <div
+                key={itemIdx}
+                className='forecast-day'>
+                <div>
+                  <p>Date: {new Date(item.date).toLocaleDateString()}</p>
+                  <p>Max Temp: {item.maxTemp} °C</p>
+                  <p>Min Temp: {item.minTemp} °C</p>
+                  <img
+                    src={item.icon}
+                    alt=''
+                  />
+                  <p>{item.text}</p>
                 </div>
-              ))}
+              </div>
+            ))}
           </div>
         </div>
-      ) : (
-        <p>Weather information unavailable.</p>
       )}
-    </div>
+    </>
   );
 }
 
